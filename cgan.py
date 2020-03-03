73964a3a25502557f0f772ce6ec355f345702913
Deep Ganguli <deep.ganguli@gmail.com>
2020-03-03 14:40:48 -0800

    cgan initial commit

---
+++ cgan.py
@@ -0,0 +1,171 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim, img_shape):
+        super(Generator, self).__init__()
+
+        # set to 10 for 10 digits
+        self.label_emb = nn.Embedding(num_embeddings=10, embedding_dim=10)
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(latent_dim + 10, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, noise, labels):
+        # Concatenate label embedding and image to produce input
+        gen_input = torch.cat((self.label_emb(labels), noise), -1)
+        img = self.model(gen_input)
+        img_shape = (1, 28, 28)
+        img = img.view(img.size(0), *img_shape)
+        return img
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_shape):
+        super(Discriminator, self).__init__()
+
+        self.label_embedding = nn.Embedding(10, 10)
+
+        self.model = nn.Sequential(
+            nn.Linear(10 + int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.Dropout(0.4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 512),
+            nn.Dropout(0.4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 1),
+        )
+
+    def forward(self, img, labels):
+        # Concatenate label embedding and image to produce input
+        d_in = torch.cat((img.view(img.size(0), -1), self.label_embedding(labels)), -1)
+        validity = self.model(d_in)
+        return validity
+
+
+class CGANTrainer:
+    def __init__(self,
+                 train_loader,
+                 save_path=None,
+                 latent_dim=100,
+                 img_shape=(1, 28, 28),
+                 n_epochs=200,
+                 learning_rate=0.0002,
+                 b1=0.5,
+                 b2=0.999):
+
+        self.latent_dim = latent_dim
+        self.train_loader = train_loader
+        self.n_epochs = n_epochs
+
+        # Initialize generator and discriminator
+        self.generator = Generator(latent_dim, img_shape)
+        self.discriminator = Discriminator(img_shape)
+
+        # loss functions
+        self.adversarial_loss = torch.nn.MSELoss()
+
+        # handle gpu
+        cuda = True if torch.cuda.is_available() else False
+
+        if cuda:
+            print("Sending models to gpu")
+            self.generator.cuda()
+            self.discriminator.cuda()
+            self.adversarial_loss.cuda()
+
+        self.FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+        self.LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
+
+        # optimizers
+        self.optimizer_G = torch.optim.Adam(self.generator.parameters(), lr=learning_rate, betas=(b1, b2))
+        self.optimizer_D = torch.optim.Adam(self.discriminator.parameters(), lr=learning_rate, betas=(b1, b2))
+
+    def train(self):
+        for epoch in range(self.n_epochs):
+            for i, (imgs, labels) in enumerate(self.train_loader):
+                batch_size = imgs.shape[0]
+
+                # Adversarial ground truths
+                valid = Variable(self.FloatTensor(batch_size, 1).fill_(1.0), requires_grad=False)
+                fake = Variable(self.FloatTensor(batch_size, 1).fill_(0.0), requires_grad=False)
+
+                # Configure input
+                real_imgs = Variable(imgs.type(self.FloatTensor))
+                labels = Variable(labels.type(self.LongTensor))
+
+                # -----------------
+                #  Train Generator
+                # -----------------
+
+                self.optimizer_G.zero_grad()
+
+                # Sample noise and labels as generator input
+                z = Variable(self.FloatTensor(np.random.normal(0, 1, (batch_size, self.latent_dim))))
+                gen_labels = Variable(self.LongTensor(np.random.randint(0, 10, batch_size)))
+
+                # Generate a batch of images
+                gen_imgs = self.generator(z, gen_labels)
+
+                # Loss measures generator's ability to fool the discriminator
+                validity = self.discriminator(gen_imgs, gen_labels)
+                g_loss = self.adversarial_loss(validity, valid)
+
+                g_loss.backward()
+                self.optimizer_G.step()
+
+                # ---------------------
+                #  Train Discriminator
+                # ---------------------
+
+                self.optimizer_D.zero_grad()
+
+                # Loss for real images
+                validity_real = self.discriminator(real_imgs, labels)
+                d_real_loss = self.adversarial_loss(validity_real, valid)
+
+                # Loss for fake images
+                validity_fake = self.discriminator(gen_imgs.detach(), gen_labels)
+                d_fake_loss = self.adversarial_loss(validity_fake, fake)
+
+                # Total discriminator loss
+                d_loss = (d_real_loss + d_fake_loss) / 2
+
+                d_loss.backward()
+                self.optimizer_D.step()
+
+                print(
+                    "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+                    % (epoch, self.n_epochs, i, len(self.train_loader), d_loss.item(), g_loss.item())
+                )
+
+if __name__ == '__main__':
+    from data import load_mnist
+    import time
+
+    train_loader, test_loader = load_mnist('/tmp')
+    trainer = CGANTrainer(train_loader, save_path='/tmp')
+    start = time.time()
+    trainer.train(100)
+    end = time.time()
+    elapsed = end - start
+    print(elapsed)
