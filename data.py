e6f40358551d1ec565c74edd96ee3953e26db8a4
Deep Ganguli <deep.ganguli@gmail.com>
2020-03-04 10:45:52 -0800

    pytorch data loader 403 work around

--- data.py
+++ data.py
@@ -1,8 +1,14 @@
 import torch
 import torchvision
+from six.moves import urllib
 
 
 def load_mnist(save_path, batch_size_train=64, batch_size_test=1000):
+    # this is a temporary work around to pytorch throwing 403s
+    # see: https://github.com/pytorch/vision/issues/1938
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    urllib.request.install_opener(opener)
 
     train_loader = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST(save_path,
