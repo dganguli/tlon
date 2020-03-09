bc7d9a49f6b063e031e566f99958ccde01ca3e8c
Deep Ganguli <deep.ganguli@gmail.com>
2020-03-09 16:01:35 -0700

    cnn: use nn.sequential

--- cnn.py
+++ cnn.py
@@ -1,4 +1,5 @@
 import os
+from collections import OrderedDict
 
 import torch
 import torch.nn as nn
@@ -20,45 +21,27 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
 
         self.model = nn.Sequential(
-            nn.Conv2d(1, 10, kernel_size=5),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            nn.Conv2d(10, 20, kernel_size=5),
-            nn.Dropout2d(),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-            View((-1, 320)),
-            nn.Linear(320, 50),
-            nn.Dropout(),
-            nn.Linear(50, 10),
-            nn.LogSoftmax(dim=1)
+            OrderedDict([
+                ('conv_1', nn.Conv2d(1, 10, kernel_size=5)),
+                ('max_pool2d_1', nn.MaxPool2d(2)),
+                ('relu_1', nn.ReLU()),
+                ('conv_2', nn.Conv2d(10, 20, kernel_size=5)),
+                ('dropout_2d', nn.Dropout2d()),
+                ('max_pool2d_2', nn.MaxPool2d(2)),
+                ('relu_2', nn.ReLU()),
+                ('reshape', View((-1, 320))),
+                ('fc1', nn.Linear(320, 50)),
+                ('dropout_1d', nn.Dropout()),
+                ('fc2', nn.Linear(50, 10)),
+                ('log_softmax', nn.LogSoftmax(dim=1))
+            ])
         )
-
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         return self.model(x)
 
-    def forward_old(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = F.relu(self.fc1(x.view(-1, 320)))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, 1)
-
-    def fc1_layer_activation(self, x):
-        self.eval()
-        layers = dict(self.named_children())
-        x = F.relu(F.max_pool2d(layers['conv1'](x), 2))
-        x = F.relu(F.max_pool2d(layers['conv2_drop'](layers['conv2'](x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(layers['fc1'](x))
-        return x
+    def get_activation_layer(self, x, layer_name):
+        pass
 
     def from_save_dict(self, model_path):
         self.load_state_dict(torch.load(model_path))
@@ -157,11 +140,11 @@ class CNNTrainer:
 if __name__ == '__main__':
     from data import load_mnist
     import time
+
     train_loader, test_loader = load_mnist('/tmp')
     trainer = CNNTrainer(train_loader, test_loader, save_path='/tmp')
     start = time.time()
     trainer.train(100)
     end = time.time()
-    elapsed = end-start
+    elapsed = end - start
     print(elapsed)
-
