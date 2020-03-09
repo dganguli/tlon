bee68955e391e714d6e1ef31576801213028596d
Deep Ganguli <deep.ganguli@gmail.com>
2020-03-09 13:59:18 -0700

    cnn: use sequential instead of functional api

--- cnn.py
+++ cnn.py
@@ -6,9 +6,34 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 
+class View(nn.Module):
+    def __init__(self, shape):
+        super(View, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.view(*self.shape)
+
+
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 10, kernel_size=5),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            nn.Conv2d(10, 20, kernel_size=5),
+            nn.Dropout2d(),
+            nn.MaxPool2d(2),
+            nn.ReLU(),
+            View((-1, 320)),
+            nn.Linear(320, 50),
+            nn.Dropout(),
+            nn.Linear(50, 10),
+            nn.LogSoftmax(dim=1)
+        )
+
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
@@ -16,12 +41,24 @@ class CNN(nn.Module):
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
+        return self.model(x)
+
+    def forward_old(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = F.relu(self.fc1(x.view(-1, 320)))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, 1)
+
+    def fc1_layer_activation(self, x):
+        self.eval()
+        layers = dict(self.named_children())
+        x = F.relu(F.max_pool2d(layers['conv1'](x), 2))
+        x = F.relu(F.max_pool2d(layers['conv2_drop'](layers['conv2'](x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(layers['fc1'](x))
+        return x
 
     def from_save_dict(self, model_path):
         self.load_state_dict(torch.load(model_path))
@@ -44,11 +81,15 @@ class CNNTrainer:
 
         self.n_epochs = n_epochs
 
-        self.CNN = CNN().cuda()
+        self.CNN = CNN()
 
         self.optimizer = optim.SGD(self.CNN.parameters(),
                                    lr=learning_rate,
                                    momentum=momentum)
+
+        self.cuda = True if torch.cuda.is_available() else False
+        if self.cuda:
+            self.CNN.cuda()
 
         self.train_losses = []
         self.train_counter = []
@@ -62,8 +103,9 @@ class CNNTrainer:
 
         for batch_idx, (data, target) in enumerate(self.train_loader):
             self.optimizer.zero_grad()
-            data = data.cuda()
-            target = target.cuda()
+            if self.cuda:
+                data = data.cuda()
+                target = target.cuda()
             output = self.CNN(data)
             loss = F.nll_loss(output, target)
             loss.backward()
@@ -91,8 +133,9 @@ class CNNTrainer:
 
         with torch.no_grad():
             for data, target in self.test_loader:
-                data = data.cuda()
-                target = target.cuda()
+                if self.cuda:
+                    data = data.cuda()
+                    target = target.cuda()
                 output = self.CNN(data)
                 test_loss += F.nll_loss(output, target, reduction='sum').item()
                 pred = output.data.max(1, keepdim=True)[1]
